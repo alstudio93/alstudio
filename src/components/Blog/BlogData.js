87ea840e6570394271bec9eb47c17b06d47175c4
alstudio93 <83974894+alstudio93@users.noreply.github.com>
2022-09-14 08:47:10 -0500

    Update BlogData.js

--- src/components/Blog/BlogData.js
+++ src/components/Blog/BlogData.js
@@ -1,15 +1,17 @@
 export const PostData = [
-    {
+   /* {
         image: "/assets/blog/accordion.webp",
         title: "Build an Accordion with ReactJS & Tailwind CSS",
         description: "Let's build an Accordion using ReactJS and Tailwind CSS! Since dark mode is such a desirable feature, I wanted to include that in the build!",
         link: "https://medium.com/@andrew.laddstudio/lets-make-an-accordion-with-nextjs-tailwind-css-and-implement-dark-mode-e6241cbc4e27",
     },
-/*    {
+    
+   */
+   {
         image: "/assets/blog/from-music-to-code.png",
         title: "From Music to Code",
         description: "This is my journey from becoming a music student to a self-taught web developer.",
         link: "https://medium.com/@andrew.laddstudio/from-music-to-code-e7dfe7b800a7",
     },
-*/
+
 ]
